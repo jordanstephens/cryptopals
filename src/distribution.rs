56f036461c5d4e459ed84c8d4930ed73f2c92a68
Jordan Stephens <jordan@stephens.io>
2020-01-12 21:07:26 -0600

    challenge 4

--- src/distribution.rs
+++ src/distribution.rs
@@ -1,51 +1,195 @@
-use std::collections::BTreeMap;
+use std::collections::HashMap;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Key {
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z,
+  WHITESPACE,
+  OTHER,
+  UNKNOWN,
+}
+
+const ENGLISH: [(Key, f32, &str); 29] = [
+  (Key::A, 0.05213904, "a"),
+  (Key::B, 0.00993984, "b"),
+  (Key::C, 0.01738712, "c"),
+  (Key::D, 0.02798680, "d"),
+  (Key::E, 0.08331536, "e"),
+  (Key::F, 0.01583048, "f"),
+  (Key::G, 0.01268880, "g"),
+  (Key::H, 0.03943104, "h"),
+  (Key::I, 0.04464752, "i"),
+  (Key::J, 0.00072264, "j"),
+  (Key::K, 0.00404232, "k"),
+  (Key::L, 0.02651920, "l"),
+  (Key::M, 0.01616992, "m"),
+  (Key::N, 0.04516104, "n"),
+  (Key::O, 0.04770416, "o"),
+  (Key::P, 0.01101160, "p"),
+  (Key::Q, 0.00068848, "q"),
+  (Key::R, 0.03980504, "r"),
+  (Key::S, 0.04126080, "s"),
+  (Key::T, 0.05834856, "t"),
+  (Key::U, 0.01801072, "u"),
+  (Key::V, 0.00663224, "v"),
+  (Key::W, 0.01370176, "w"),
+  (Key::X, 0.00109536, "x"),
+  (Key::Y, 0.01167872, "y"),
+  (Key::Z, 0.00062688, "z"),
+  (Key::WHITESPACE, 0.16666666, "\n\t "),
+  (
+    Key::OTHER,
+    0.0333333,
+    "0123456789-_=+()*&^%$#@!~`:;,./<>?|\"\'\\",
+  ),
+  (Key::UNKNOWN, 0.0, ""),
+];
 
 pub struct Distribution {
-  pub table: BTreeMap<char, f32>,
+  pub table: HashMap<Key, f32>,
+  lookup: HashMap<char, Key>,
 }
 
 impl Distribution {
-  fn new() -> Self {
-    Distribution {
-      table: BTreeMap::new(),
-    }
+  pub fn new() -> Self {
+    let table = HashMap::new();
+    let lookup = HashMap::new();
+    Distribution { table, lookup }
   }
 
-  pub fn from(input: &str) -> Self {
+  pub fn from(input: &[u8]) -> Self {
     let mut distribution = Distribution::empty();
     let len = input.len() as f32;
-    for car in input.chars() {
-      let key = car.to_ascii_lowercase();
-      if distribution.table.contains_key(&key) {
-        distribution.table.insert(
-          key,
-          distribution.table.get(&key).unwrap_or(&0.0) + (1.0 / len),
-        );
-      }
+    for byte in input {
+      let lower_char = (*byte as char).to_ascii_lowercase();
+      let key = distribution
+        .lookup
+        .get(&lower_char)
+        .unwrap_or(&Key::UNKNOWN);
+      let curr = distribution.table.get(key).unwrap();
+      distribution.table.insert(*key, curr + (1.0 / len));
     }
     distribution
   }
 
   pub fn empty() -> Self {
-    let keys = "abcdefghijklmnopqrstuvwxyz".chars();
     let mut distribution = Distribution::new();
-    for key in keys {
-      distribution.table.insert(key, 0.0);
+    for (key, _freq, chars) in ENGLISH.iter() {
+      distribution.table.insert(*key, 0.0);
+      for car in chars.chars() {
+        distribution.lookup.insert(car, *key);
+      }
     }
     distribution
   }
 
   pub fn english() -> Self {
-    let keys = "abcdefghijklmnopqrstuvwxyz".chars();
-    let frequencies: Vec<f32> = vec![
-      0.08167, 0.01492, 0.02202, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153,
-      0.01292, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09356,
-      0.02758, 0.00978, 0.02560, 0.00150, 0.01994, 0.00077,
-    ];
     let mut distribution = Distribution::new();
-    for (key, freq) in keys.zip(frequencies.into_iter()) {
-      distribution.table.insert(key, freq);
+    for (key, freq, chars) in ENGLISH.iter() {
+      distribution.table.insert(*key, *freq);
+      for car in chars.chars() {
+        distribution.lookup.insert(car, *key);
+      }
     }
     distribution
   }
+
+  pub fn scale(&mut self, n: f32) -> &Self {
+    self.table.iter_mut().map(|(_, value)| *value * n);
+    self
+  }
+}
+
+pub fn score(sample: &[u8]) -> f32 {
+  let len = sample.len() as f32;
+  let mut observed = Distribution::from(&sample);
+  let mut english = Distribution::english();
+  distance(&observed.scale(len), &english.scale(len))
+}
+
+pub fn distance(observed: &Distribution, expected: &Distribution) -> f32 {
+  observed.table.keys().fold(0.0, |acc, key| {
+    let o = observed.table.get(key).unwrap();
+    let e = expected.table.get(key).unwrap();
+    acc + chi_squared_i(*o, *e)
+  })
+}
+
+fn chi_squared_i(observed: f32, expected: f32) -> f32 {
+  if expected == 0.0 && observed == 0.0 {
+    return 0.0;
+  }
+  if expected == 0.0 && observed != 0.0 {
+    return std::f32::INFINITY;
+  }
+  (observed - expected).powi(2) / expected
+}
+
+fn chi_squared(observed: &Vec<f32>, expected: &Vec<f32>) -> f32 {
+  observed
+    .iter()
+    .zip(expected.iter())
+    .fold(0.0, |acc, (&o, &e)| acc + chi_squared_i(o, e))
+}
+
+#[test]
+fn test_chi_squared_eq() {
+  let o = vec![1.0, 2.0, 3.0];
+  let e = vec![1.0, 2.0, 3.0];
+  assert_eq!(0.0, chi_squared(&o, &e));
+}
+
+#[test]
+fn test_chi_squared_zero_expected() {
+  let o = vec![1.0, 2.0];
+  let e = vec![0.0, 2.0];
+  let result = chi_squared(&o, &e);
+  assert_eq!(std::f32::INFINITY, result);
+}
+
+#[test]
+fn test_chi_squared_ne() {
+  let o = vec![0.0, 1.0];
+  let e = vec![1.0, 1.0];
+  let result = chi_squared(&o, &e);
+  assert_eq!(1.0, result);
+}
+
+#[test]
+fn test_chi_squared_inverted() {
+  let o = vec![1.0, 2.0];
+  let e = vec![2.0, 1.0];
+  let result = chi_squared(&o, &e);
+  assert_eq!(1.5, result);
+}
+
+#[test]
+fn test_score() {
+  let a = "Now that the party is jumping\n";
+  let b = "\u{1a};#t <5 t <1t$5& -t=\'t>!9$=:3^";
+  assert!(score(a.as_bytes()) < score(b.as_bytes()));
 }
--- src/xor_cipher.rs
+++ src/xor_cipher.rs
@@ -1,67 +1,86 @@
+use super::distribution::score;
 use ordered_float::OrderedFloat;
 use rayon::prelude::*;
-use super::buffer::Buffer;
-use super::distribution::Distribution;
 
-pub fn decrypt(buffer: &Buffer, key: &u8) -> String {
-  let out: Vec<u8> = buffer.bytes.iter().map(|byte| byte ^ key).collect();
-  String::from_utf8_lossy(out.as_slice()).to_string()
+pub fn decrypt(bytes: &[u8], key: &[u8]) -> Vec<u8> {
+  bytes
+    .iter()
+    .zip(key.iter().cycle())
+    .map(|(byte, key)| byte ^ key)
+    .collect()
 }
 
-#[derive(Debug)]
-pub struct FindKeyResult {
+#[derive(Debug, Clone)]
+pub struct KeyCandidate {
   key: u8,
   score: f32,
-  plaintext: String
+  plaintext: Vec<u8>,
 }
 
-pub fn find_key(buffer: &Buffer) -> FindKeyResult {
-  (0..256).into_par_iter().map(|i| {
-    let key = i as u8;
-    let plaintext = decrypt(&buffer, &key);
-    let score = score(&plaintext);
-    FindKeyResult { key, score, plaintext }
-  })
-  .min_by_key(|result| OrderedFloat(result.score))
-  .unwrap()
+pub fn find_candidate_keys(ciphertext: &[u8]) -> Vec<KeyCandidate> {
+  (0..255u8)
+    .into_par_iter()
+    .map(|key| {
+      let plaintext = decrypt(&ciphertext, &[key]);
+      let score = score(&plaintext[..]);
+      KeyCandidate {
+        key,
+        score,
+        plaintext,
+      }
+    })
+    .collect()
 }
 
-fn score(sample: &str) -> f32 {
-  let english = Distribution::english();
-  let distribution = Distribution::from(&sample);
-  let len = sample.len() as f32;
-  let observed: Vec<f32> = distribution.table.values().map(|val| val * len).collect();
-  let expected: Vec<f32> = english.table.values().map(|val| val * len).collect();
-  chi_squared(observed, expected)
-}
-
-fn chi_squared(observed: Vec<f32>, expected: Vec<f32>) -> f32 {
-  observed
-    .iter()
-    .zip(expected.iter())
-    .fold(0.0, |acc, (o, e)| acc + (o - e).powi(2) / e)
+pub fn find_best_key(bytes: &[u8]) -> KeyCandidate {
+  find_candidate_keys(&bytes)
+    .into_iter()
+    .min_by_key(|r| OrderedFloat(r.score))
+    .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
+  use super::super::decoder;
   use super::*;
   use std::fs::File;
   use std::io::{prelude::*, BufReader};
 
   #[test]
   fn test_decrypt() {
-    let text = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    let buffer = Buffer::from_hex(text).unwrap();
-    let key = b'X';
-    let result = decrypt(&buffer, &key);
-    assert_eq!(result, "Cooking MC\'s like a pound of bacon");
+    let ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let buffer = decoder::from_hex(ciphertext).unwrap();
+    let key = "X";
+    let plaintext = decrypt(&buffer, key.as_bytes());
+    let expected = "Cooking MC\'s like a pound of bacon".as_bytes();
+    assert_eq!(plaintext, expected);
   }
 
   #[test]
-  fn test_find_key() {
+  fn test_find_best_key() {
     let ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    let buffer = Buffer::from_hex(ciphertext).unwrap();
-    let result = find_key(&buffer);
+    let buffer = decoder::from_hex(ciphertext).unwrap();
+    let result = find_best_key(&buffer);
     assert_eq!(result.key, b'X');
+  }
+
+  #[test]
+  fn test_find_key_from_file() {
+    let file = File::open("data/4.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let result: KeyCandidate = reader
+      .lines()
+      .flat_map(|line| {
+        let ciphertext = line.unwrap();
+        let buffer = decoder::from_hex(&ciphertext).unwrap();
+        find_candidate_keys(&buffer)
+      })
+      .min_by_key(|result| OrderedFloat(result.score))
+      .unwrap();
+
+    let expected = "Now that the party is jumping\n".as_bytes();
+    assert_eq!(result.key, 53);
+    assert_eq!(result.plaintext, expected);
   }
 }
